c83460a2c1d4e09123382b4741176f41dd12eb56
qe27 <podlipaev.sergey@gmail.com>
2021-04-24 01:35:40 +0300

    fix

---
+++ map_objects/__init__.py
@@ -0,0 +1,4 @@
+from objects.game_objects.map_object import MapObject
+from objects.game_objects.settlements.settlement import Settlement
+from objects.game_objects.landscape import plain
+from objects.game_objects.landscape import river
